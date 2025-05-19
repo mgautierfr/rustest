01ce647f9615d1092037a86bbed392cfd5c1bf0c
Matthieu Gautier <mgautier@kymeria.fr>
2025-05-19 16:17:15 +0200

    Add test on global + tempfile

--- rustest-testing/tests/global_test.rs
+++ rustest-testing/tests/global_test.rs
@@ -20,5 +20,30 @@ fn test_global_1(number: Global<IncNumber>) {
     assert_eq!(*number, 0);
 }
 
+fn run(options: Option<&[&str]>) -> std::io::Result<std::process::Output> {
+    let exec = env!("CARGO_BIN_EXE_global_test");
+    let mut command = std::process::Command::new(&exec);
+    command.env("NO_COLOR", "1");
+    options.map(|options| {
+        for opt in options {
+            command.arg(opt);
+        }
+    });
+    command.output()
+}
+
+#[test]
+fn test_global_tempdir() {
+    let output = run(None).unwrap();
+    let stderr = String::from_utf8_lossy(&output.stderr);
+    let outlines = stderr
+        .split('\n')
+        .filter(|l| !l.is_empty())
+        .collect::<Vec<_>>();
+    println!("{:?}", outlines);
+    assert_eq!(outlines.len(), 2);
+    assert_eq!(outlines[0], outlines[1]);
+}
+
 #[main]
 fn main() {}
